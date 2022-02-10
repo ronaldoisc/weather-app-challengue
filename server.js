d9df930b0085b9eb71730e95be3778a992e1447b
ronaldoisc <tellezleal9928@gmail.com>
2022-02-09 21:45:31 -0600

    change a route in the server.js

--- server.js
+++ server.js
@@ -7,7 +7,9 @@ app.use(express.static(path.join(__dirname, 'build')));
 app.get('/ping', function (req, res) {
  return res.send('pong');
 });
-
+app.get("/sw.js", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "build", "sw.js"));
+  });
 app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
