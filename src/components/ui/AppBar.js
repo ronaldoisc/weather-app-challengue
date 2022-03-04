b3f755cbe8dd58299f9ba8022da8e05d6eac1077
ronaldoisc <tellezleal9928@gmail.com>
2022-03-04 14:54:12 -0600

    commit test4

--- src/components/ui/AppBar.js
+++ src/components/ui/AppBar.js
@@ -11,7 +11,7 @@ export const AppBar = () => {
     }
     return (
         <div className='d-flex justify-content-around mt-5'>
-             <button onClick={handleClickInput} className='btn-search'>Search for places test</button>
+             <button onClick={handleClickInput} className='btn-search'>Search for places test4</button>
              <button className='material-icons md-light btn-gps'>gps_fixed</button>
         </div>
     )
